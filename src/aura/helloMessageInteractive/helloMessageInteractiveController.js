93a330dc66c1dc334ec3b07e23e926a246744ae6
nakao1043 <nakao1043@yahoo.co.jp>
2021-12-23 15:00:37 +0900

    commit

---
+++ src/aura/helloMessageInteractive/helloMessageInteractiveController.js
@@ -0,0 +1,10 @@
+({
+    myAction : function(component, event, helper) {
+
+    },
+    handleClick : function(component, event, helper){
+        let btnClicked = event.getSource();         // the button
+        let btnMessage = btnClicked.get("v.label"); // the button's label
+        component.set("v.message", btnMessage);     // update our message
+    }
+})
